94048bce31b2dbd7e31a97c3d2972cb82e93d9b9
ol-dev0003 <nicolaso@ortegalive.com>
2023-11-07 14:59:03 -0500

    added link to about us page

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import cartIcon from '../imgs/icons8-cart-64.png';
 import { useSelector } from "react-redux";
 
-const Navbar = () => {
+const Navbar = () =>
+{
     const cartItems = useSelector(state => state.cart.items);
     const cartCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
     const displayCount = cartCount > 10 ? '10+' : cartCount;
@@ -13,14 +14,14 @@ const Navbar = () => {
             <ul className="nav-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/categories">Categories</Link></li>
-                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/about-us">About Us</Link></li>
                 <li>
-                <Link to="/cart" className="cart-icon-link">
-                    <div className="cart-icon-container">
-                        <img src={cartIcon} alt="Cart Icon" />
-                        {cartCount > 0 && <span className={`cart-number ${cartCount > 9 ? 'double-digit' : ''}`}>{displayCount}</span>}
-                    </div>
-                </Link>
+                    <Link to="/cart" className="cart-icon-link">
+                        <div className="cart-icon-container">
+                            <img src={cartIcon} alt="Cart Icon" />
+                            {cartCount > 0 && <span className={`cart-number ${cartCount > 9 ? 'double-digit' : ''}`}>{displayCount}</span>}
+                        </div>
+                    </Link>
 
                 </li>
             </ul>
